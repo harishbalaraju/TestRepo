bbd0cdaaca1044efe79721b9d3f4c3e558ff883d
Harish B <b.harish2000@gmail.com>
2021-12-30 13:21:05 +0000

    added Numbers Java file

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers
+{
+        public static void main(String args[])
+        {
+                int i;
+                for(i=0;i<=10;i++)
+                {
+                        System.out.println("Number " + i);
+                }
+
+        }
+}
+
